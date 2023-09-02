c7bc19eeae5e1f573494c700f394fd0a7ccc7e65
AbisheakJacob <abisheakjacob0032@gmail.com>
2023-09-02 16:52:10 +0530

    Create main.py

---
+++ np_down/main.py
@@ -0,0 +1,8 @@
+# importing the packages
+import streamlit as st
+
+# page title
+st.title("Newspaper Downloader")
+
+if st.button("Download Economic Times"):
+    st.text("Wait for the programmer to finish programming")
