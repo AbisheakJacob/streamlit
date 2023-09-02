f6de60242e9032e86c9083ba5eb72f17f181aef1
AbisheakJacob <abisheakjacob0032@gmail.com>
2023-09-02 17:08:00 +0530

    Update main.py

--- np_down/main.py
+++ np_down/main.py
@@ -1,8 +1,34 @@
 # importing the packages
 import streamlit as st
+from tkinter import *
+from tkinter import ttk
+from bs4 import BeautifulSoup
+import gdown
+import os
+import datetime
+import time
+from selenium import webdriver
 
 # page title
 st.title("Newspaper Downloader")
 
 if st.button("Download Economic Times"):
     st.text("Wait for the programmer to finish programming")
+
+    # initialize the browser
+    browser = webdriver.Chrome()
+
+    # the base url for the site
+    url = "https://epaperpdf.download/the-economic-times/"
+
+    # go to the required page
+    browser.get(url)
+
+    # wait for 3 seconds
+    time.sleep(5)
+
+    # get the html_source of the webpage
+    html_source = browser.page_source
+
+    # close the web browser
+    browser.quit()
